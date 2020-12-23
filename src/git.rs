ff6bd7e1630bc2c4880f116893d511ae1c272f3a
Sean Chen <skypemaster007@gmail.com>
2020-12-23 11:37:26 -0600

    Fetch ssh credentials

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
+use dialoguer::Password;
 use git2::{Cred, Repository, RemoteCallbacks};
 use std::{
-  env,
-  path::PathBuf,
+    env,
+    path::{Path, PathBuf},
 };
 
 pub(crate) fn clone(
@@ -10,5 +11,32 @@ pub(crate) fn clone(
     target_dir: &PathBuf, 
     needs_password: bool
 ) -> Result<()> {
-  
+    if repo.contains("http") {
+        Repository::clone(repo, &target_dir)?;
+    } else {
+        let password = match needs_password {
+            true => {
+                Password::new()
+                    .with_prompt("Enter password for .ssh/rsa_id")
+                    .interact()?
+                    .into()
+            },
+            false => None,
+        };
+
+        let mut callbacks = RemoteCallbacks::new();
+        callbacks.credentials(move |_url, username, _allowed| {
+            Cred::ssh_key(
+                username.unwrap(),
+                None,
+                Path::new(&format!(
+                    "{}/.ssh/id_rsa",
+                    env::var("HOME").expect("Cannot fetch $HOME")
+                )),
+                password.as_deref(),
+            )
+        });
+    }
+
+    Ok(())
 }
