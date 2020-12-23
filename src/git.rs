ec842dc480e0e3bbb1c72b9df4778a253328bc41
Sean Chen <skypemaster007@gmail.com>
2020-12-23 13:30:56 -0600

    Implement `create_dir` fn

--- src/git.rs
+++ src/git.rs
@@ -1,29 +1,14 @@
 use anyhow::Result;
-use dialoguer::Password;
 use git2::{Cred, Repository, RemoteCallbacks};
 use std::{
     env,
     path::{Path, PathBuf},
 };
 
-pub(crate) fn clone(
-    repo: &str, 
-    target_dir: &PathBuf, 
-    needs_password: bool
-) -> Result<()> {
+pub(crate) fn clone(repo: &str, target_dir: &PathBuf) -> Result<()> {
     if repo.contains("http") {
         Repository::clone(repo, &target_dir)?;
     } else {
-        let password = match needs_password {
-            true => {
-                Password::new()
-                    .with_prompt("Enter password for .ssh/rsa_id")
-                    .interact()?
-                    .into()
-            },
-            false => None,
-        };
-
         let mut callbacks = RemoteCallbacks::new();
         callbacks.credentials(move |_url, username, _allowed| {
             Cred::ssh_key(
@@ -33,11 +18,11 @@ pub(crate) fn clone(
                     "{}/.ssh/id_rsa",
                     env::var("HOME").expect("Cannot fetch $HOME")
                 )),
-                password.as_deref(),
+                None,
             )
         });
 
-        let foptions = git2::FetchOptions::new();
+        let mut foptions = git2::FetchOptions::new();
         foptions.remote_callbacks(callbacks);
 
         let mut repo_builder = git2::build::RepoBuilder::new();
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 mod git;
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Result, Context};
 use clap::{App, Arg, ArgMatches};
 use std::{
-    collections::HashMap,
     env,
     io::Read,
     fs::{self, File},
@@ -11,8 +10,9 @@ use std::{
     string::ToString,
 };
 use serde::{Serialize, Deserialize};
-use toml::Value;
-use dialoguer::{Input, Select};
+use dialoguer::Input;
+use walkdir::WalkDir;
+use heck::KebabCase;
 
 const CONFIG: &str = ".temploy.toml";
 
@@ -38,7 +38,7 @@ impl ProjectParameters {
                 }
 
                 fs::create_dir_all(&temp_dir)?;
-                git::clone(&template_path, &temp_dir, matches.is_present("passphrase"))?;
+                git::clone(&template_path, &temp_dir)?;
 
                 template_path = temp_dir.to_string_lossy().to_string();
             }
@@ -58,9 +58,26 @@ impl ProjectParameters {
         Ok(project_parameters)
     }
 
+    fn create_dir(&self, name: &str) -> Result<PathBuf> {
+        let mut dir_path = self.target_dir
+            .clone()
+            .unwrap_or_else(|| env::current_dir().unwrap_or_else(|_| ".".into()));
+
+        dir_path = dir_path.join(name.to_kebab_case());
+
+        if dir_path.exists() {
+            return Err(anyhow!("Failed to create {} since it already exists", dir_path.to_string_lossy()));
+        }
+
+        fs::create_dir_all(&dir_path).with_context(|| "Cannot create directory")?;
+
+        let path = fs::canonicalize(dir_path).with_context(|| "Cannot canonicalize path")?;
+
+        Ok(path)
+    }
+
     pub fn generate(&self) -> Result<()> {
-        let mut params: HashMap<String, Value> = self.fetch_params()?; 
-        let project_name = match &self.project_name {
+        let project_name = match &self.name {
             Some(name) => name.clone(),
             None => Input::new()
                 .with_prompt("Specify a name for your generated project: ")
@@ -68,9 +85,6 @@ impl ProjectParameters {
         };
 
         let dir_path = self.create_dir(&project_name)?;
-
-        params.insert(String::from("target_dir"), Value::String(dir_path.to_str().unwrap_or_default().to_string()));
-        params.insert(String::from("name", Value::String(project_name.clone()));
 
         let entries = WalkDir::new(&self.template_path)
             .into_iter()
@@ -82,10 +96,47 @@ impl ProjectParameters {
                     return false;        
                 }
 
-                if e.file_name() == CONFIG {
-                    return false;
-                }
+                !(e.file_name() == CONFIG)
             });
+        
+        println!("Generating project...");
+
+        for e in entries {
+            let entry = e.map_err(|err| anyhow!("Cannot read entry: {}", err))?;
+            let entry_path = entry.path().strip_prefix(&self.template_path)?;
+            let full_path = dir_path.join(entry_path);
+
+            if entry_path == PathBuf::from("") {
+                continue;
+            }
+
+            if entry.file_type().is_dir() {
+                if entry.path().to_str() == Some(".") {
+                    continue;
+                }
+
+                fs::create_dir(full_path)
+                    .map_err(|err| anyhow!("Cannot create directory: {}", err))?;
+                continue;
+            }
+
+            let filename = entry.path();
+            let mut content = String::new();
+
+            {
+                let mut file = File::open(filename)
+                    .map_err(|err| anyhow!("Cannot open file: {}", err))?;
+                file.read_to_string(&mut content)
+                    .map_err(|err| anyhow!("Cannot read file: {}", err))?;
+            }
+
+            fs::write(full_path, content)
+                .map_err(|err| anyhow!("Failed to write to file: {}", err))?;
+        }
+
+        println!("Project {} has been successfully generated", project_name);
+
+        Ok(())
     }
 }
 
@@ -109,11 +160,6 @@ pub fn cli_init() -> Result<()> {
                         .long("target-directory")
                         .help("Specify the target directory.")
                         .takes_value(true),
-                    Arg::with_name("password")
-                        .short("p")
-                        .long("password")
-                        .help("Specify if your SSH key is protected by a password")
-                        .takes_value(false),
                 ]),
         )
         .get_matches();
