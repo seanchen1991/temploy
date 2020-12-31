a6880d8cc0906eed9ca61f78b1cd545af52a0f2b
Sean Chen <seanchen11235@gmail.com>
2020-12-30 20:00:47 -0600

    Add some more tests

--- tests/generate.rs
+++ tests/generate.rs
@@ -1,3 +1,4 @@
+use assert_fs::TempDir;
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
@@ -12,6 +13,37 @@ fn no_template_provided() -> Result<(), Error> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("The following required arguments were not provided:"));
+
+    Ok(())
+}
+
+#[test]
+fn invalid_template_provided() -> Result<(), Error> {
+    let mut cmd = Command::cargo_bin("temploy")?;
+
+    cmd.arg("generate").arg("file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Invalid template path specified:"));
+
+    Ok(())
+}
+
+#[test]
+fn generates_template_correctly() -> Result<(), Error> {
+    let path = "tests/test-data";
+    let temp = TempDir::new().unwrap();
+    let mut cmd = Command::cargo_bin("temploy")?;
+
+    cmd.arg("generate")
+        .arg(path)
+        .arg("-d")
+        .arg(temp.path());
+
+    cmd.assert().success();
+    assert!(dir_diff::is_different(&temp.path(), path).unwrap());
+
+    temp.close().unwrap();
 
     Ok(())
 }
